523969a9f8bcd8f008ebabd9f96cb056c9cd3e43
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-02-28 14:02:08 -0500

    cleaned up boiler plate from vite and added threeJs canvas

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { OrbitControls } from "@react-three/drei";
+
+export const Home = () => {
+  return (
+    <>
+      <OrbitControls />
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </>
+  );
+};
